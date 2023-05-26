ad28cbc66e467be42fe7d7a52bbd5d93a952074f
Mialomi <e.silvasm@gmail.com>
2023-05-26 18:40:59 +0200

    ex2 definitiu

--- ex2.php
+++ ex2.php
@@ -1,28 +1,27 @@
 <?php
 //Imprimeix per pantalla "Hello, World!" utilitzant una variable.
 
-$text = "Hello, World";
+$text = "Hello, World!";
 
-echo $text;
+echo $text. "<br>";
 
 //Transforma tots els caràcters de l'string a majúscules i imprimeix en pantalla.
 
-echo strtoupper($text);
+echo strtoupper($text). "<br>";
 
 //Imprimeix per pantalla la mida (longitud) de la variable.
 
-echo strlen($text);
+echo strlen($text). "<br>";
 
 //Imprimeix per pantalla l'string en ordre invers de caràcters.
 
-echo strrev("Hello, World");
-echo strrev($text);
+echo strrev($text). "<br>";
 
 //Crea una nova variable amb el contingut “Aquest és el curs de PHP” i imprimeix per pantalla la concatenació de tots dos strings.
 
-$text2 = "Aquest és el curs de PHP";
+$text2 = " Aquest és el curs de PHP.";
 
-echo $text . $text2;
+echo $text . $text2. "<br>";
 
 
 ?>

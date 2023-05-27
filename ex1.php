c56cf2b8b38cb54baf1f4900b731b81d7d817d1b
Mialomi <e.silvasm@gmail.com>
2023-05-27 17:30:49 +0200

    ex1

---
+++ ex1.php
@@ -0,0 +1,14 @@
+<?php
+
+//Crea un array, afegeix-li 5 nombres enters i després mostrals per pantalla d’un en un.
+
+$llista = [11, 22, 33, 44, 55];
+
+echo $llista[0]. "<br>";
+echo $llista[1]. "<br>";
+echo $llista[2]. "<br>";
+echo $llista[3]. "<br>";
+echo $llista[4]. "<br>";
+
+
+?>

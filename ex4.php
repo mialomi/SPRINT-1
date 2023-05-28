c5342e98f2e9642edc6ebc99ec0a6a78d2c9f762
Mialomi <e.silvasm@gmail.com>
2023-05-28 17:31:04 +0200

    ex4 prueba commit2

--- ex4.php
+++ ex4.php
@@ -1,8 +1,12 @@
 <?php
 
+/*Fes un array associatiu que representi informació de tu mateix/a. En concret ha d’incloure:
+- nom
+- edat
+- email
+- menjar favorit*/
 
-
-
+$info = array("nom"=>"Mia", "edat"=>"34", "email"=>"hi.mialomi@gmail.com", "menjar"=>"Sushi");
 
 
 

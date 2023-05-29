85a999f0da6425f132893e3a1d071fec07ed5cf9
Mialomi <e.silvasm@gmail.com>
2023-05-29 10:40:10 +0200

    ex1 def

--- ex1.php
+++ ex1.php
@@ -2,13 +2,12 @@
 
 //Crea un array, afegeix-li 5 nombres enters i després mostrals per pantalla d’un en un.
 
-$llista = [11, 22, 33, 44, 55];
+$llista = array(11, 22, 33, 44, 55);
 
-echo $llista[0]. "<br>";
-echo $llista[1]. "<br>";
-echo $llista[2]. "<br>";
-echo $llista[3]. "<br>";
-echo $llista[4]. "<br>";
+
+foreach($llista as $nombres){
+    echo $nombres;
+}
 
 
 ?>

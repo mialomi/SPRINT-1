6a60016a61fdb91ab9330924858c92d3c6404e4f
Mialomi <e.silvasm@gmail.com>
2023-05-30 11:56:15 +0200

    creación de proyecto ex2

---
+++ Ex2/shape.php
@@ -0,0 +1,24 @@
+<?php
+
+/*Escriu un programa que defineixi una classe Shape amb un constructor que rebi com a paràmetres l'ample i alt. Defineix dues subclasses; Triangle i Rectangle que heretin de Shape i que calculin respectivament l'àrea de la forma area().*/
+
+abstract class Shape{
+    protected $ample = 0;
+    protected $alt = 0;
+
+    public function __construct($ample, $alt){
+        $this -> ample = $ample;
+        $this -> alt = $alt;
+    }
+
+    public function getAmple(){
+        return $this -> ample;
+    }
+    public function getAlt(){
+        return $this -> alt;
+    }
+
+   public abstract function calcular_area();
+}
+
+?>

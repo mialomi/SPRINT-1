38d369f0e2e2a200cec579467c1d025e955aca26
Mialomi <e.silvasm@gmail.com>
2023-06-01 18:20:35 +0200

    formulario

---
+++ ex1/datos.php
@@ -0,0 +1,12 @@
+<?php
+/*$name = $_GET["name"];
+echo "Username is " . $name;*/
+
+$name = $_POST["name"];
+echo "Username is ". $name;
+
+
+
+
+
+?>
---
+++ ex1/formulario.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ex1_form</title>
+</head>
+<body>
+    <form action="datos.php" method="POST"> 
+<!-- action es la pagina donde irá la información del form -->
+    <label for="name">
+        Name:
+        <input type="text" name="name">
+     </label>
+     <br>
+     <br>
+    <label for="email">
+        Email:
+        <input type="text" name="email">
+    </label>
+    
+    <br>
+    <br>
+    <button type="submit">Login</button>
+
+
+
+
+
+    </form>
+</body>
+</html>

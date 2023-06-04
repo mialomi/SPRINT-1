4ba1455314dc66263b461477d3ed13f51bce9801
Mialomi <e.silvasm@gmail.com>
2023-06-04 16:08:01 +0200

    ex2 inicio de sesion

---
+++ ex2/datos.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+$name = $email = "";
+
+$name = $_POST["name"];
+echo "You have entered: Username is ". $name;
+echo "<br>";
+if(!preg_match("/^[a-zA-Z-' ]*$/", $name)){
+    echo $name . " is not a valid username.";}
+else{
+    echo "and it's valid.";
+    echo "<br>";
+}
+$_SESSION["users"] = $name;
+
+
+$email = $_POST["email"];
+echo "You have entered: Email is ". $email;
+echo "<br>";
+if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+    echo $email. " is not a valid email";}
+else{
+    echo "and it's valid.";
+}
+echo "<br>";
+
+$age = $_POST["age"];
+echo "You have entered: Age is " . $age;
+echo "<br>";
+if($age <= 0 or $age > 110){
+    echo $age . " is not a valid age";}
+ else{
+    echo "and it's valid.";
+}
+echo "<br>";
+
+echo "Welcome, ". $name. "!";
+}
+
+
+
+?>
---
+++ ex2/formulario.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ex1_form</title>
+</head>
+<body>
+    <form action="datos.php" method=<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        Name:
+        <input type="text" name="name">
+     <br>
+     <br>
+        Email:
+        <input type="text" name="email">
+    <br>
+    <br>
+        Age:
+        <input type="number" name="age">
+    <br>
+    <br>
+    <input type="submit">
+    </form>
+</body>
+</html>

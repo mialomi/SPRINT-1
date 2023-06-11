67c669d969a77ef73e75f6308f53a26aa4eaa420
Mialomi <e.silvasm@gmail.com>
2023-06-11 18:03:36 +0200

    functions

---
+++ nivell2/ex2.php
@@ -0,0 +1,26 @@
+<?php
+
+/*Prograna que permeti fer calculs de total de compres*/
+
+function candyShop() {
+
+    function choco($unitC) {
+        $subChoco = $unitC * 1;
+        return $subChoco;}
+
+    function gum($unitG) {
+        $subGum = $unitG * 0.50;
+        return $subGum;}
+
+    function caramel($unitCar){
+        $subCar = $unitCar * 1.50;
+        return $subCar;}
+    }
+
+
+
+
+
+
+
+?>
